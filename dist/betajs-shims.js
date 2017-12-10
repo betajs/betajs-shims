d5c08b55d05482a22be20485988ea68846d40737
Oliver Friedmann <oliver.friedmann@gmail.com>
2017-12-09 19:27:40 -0500

    Array.prototype.find

--- dist/betajs-shims.js
+++ dist/betajs-shims.js
@@ -1,5 +1,5 @@
 /*!
-betajs-shims - v0.0.13 - 2017-09-09
+betajs-shims - v0.0.14 - 2017-12-09
 Copyright (c) Oliver Friedmann
 Apache-2.0 Software License.
 */
@@ -463,6 +463,49 @@ Apache-2.0 Software License.
                 }
             }
             return value;
+        };
+    }
+
+    // https://tc39.github.io/ecma262/#sec-array.prototype.find
+    if (!this.find) {
+        this.find = function(predicate) {
+            // 1. Let O be ? ToObject(this value).
+            if (this === null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            var o = Object(this);
+
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+
+            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+
+            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+            var thisArg = arguments[1];
+
+            // 5. Let k be 0.
+            var k = 0;
+
+            // 6. Repeat, while k < len
+            while (k < len) {
+                // a. Let Pk be ! ToString(k).
+                // b. Let kValue be ? Get(O, Pk).
+                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+                // d. If testResult is true, return kValue.
+                var kValue = o[k];
+                if (predicate.call(thisArg, kValue, k, o)) {
+                    return kValue;
+                }
+                // e. Increase k by 1.
+                k++;
+            }
+
+            // 7. Return undefined.
+            return undefined;
         };
     }
 
