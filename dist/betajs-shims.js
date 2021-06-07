6559c8bd94f005897b3adc1103d274c0cd5d7f82
Oliver Friedmann <oliver.friedmann@gmail.com>
2021-06-06 23:29:00 -0400

    Merge

--- dist/betajs-shims.js
+++ dist/betajs-shims.js
@@ -1,5 +1,5 @@
 /*!
-betajs-shims - v0.0.15 - 2019-04-16
+betajs-shims - v0.0.16 - 2021-06-06
 Copyright (c) Oliver Friedmann,Pablo Iglesias
 Apache-2.0 Software License.
 */
@@ -76,17 +76,17 @@ Apache-2.0 Software License.
                 throw new TypeError();
 
             var len = this.length >>> 0,
-                res = new Array(len), // preallocate array
-                t = this, c = 0, i = -1;
+              res = new Array(len), // preallocate array
+              t = this, c = 0, i = -1;
             if (thisArg === undefined) {
                 while (++i !== len)
-                    // checks to see if the key was set
+                  // checks to see if the key was set
                     if (i in this)
                         if (func(t[i], i, t))
                             res[c++] = t[i];
             } else {
                 while (++i !== len)
-                    // checks to see if the key was set
+                  // checks to see if the key was set
                     if (i in this)
                         if (func.call(thisArg, t[i], i, t))
                             res[c++] = t[i];
@@ -192,11 +192,11 @@ Apache-2.0 Software License.
         this.reduce =function(callback /*, initialValue*/) {
             if (this === null) {
                 throw new TypeError( 'Array.prototype.reduce ' +
-                    'called on null or undefined' );
+                  'called on null or undefined' );
             }
             if (typeof callback !== 'function') {
                 throw new TypeError( callback +
-                    ' is not a function');
+                  ' is not a function');
             }
 
             // 1. Let O be ? ToObject(this value).
@@ -220,7 +220,7 @@ Apache-2.0 Software License.
                 //    throw a TypeError exception.
                 if (k >= len) {
                     throw new TypeError( 'Reduce of empty array ' +
-                        'with no initial value' );
+                      'with no initial value' );
                 }
                 value = o[k++];
             }
@@ -346,8 +346,8 @@ Apache-2.0 Software License.
             }
 
             var n, k,
-                t = Object(this),
-                len = t.length >>> 0;
+              t = Object(this),
+              len = t.length >>> 0;
             if (len === 0) {
                 return -1;
             }
@@ -512,6 +512,55 @@ Apache-2.0 Software License.
     if (!Array.isArray) {
         Array.isArray = function(arg) {
             return Object.prototype.toString.call(arg) === '[object Array]';
+        };
+    }
+
+    if (!this.includes) {
+        // https://tc39.github.io/ecma262/#sec-array.prototype.includes
+        this.includes = function(searchElement, fromIndex) {
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            // 1. Let O be ? ToObject(this value).
+            var o = Object(this);
+
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+
+            // 3. If len is 0, return false.
+            if (len === 0) {
+                return false;
+            }
+
+            // 4. Let n be ? ToInteger(fromIndex).
+            //    (If fromIndex is undefined, this step produces the value 0.)
+            var n = fromIndex | 0;
+
+            // 5. If n ≥ 0, then
+            //  a. Let k be n.
+            // 6. Else n < 0,
+            //  a. Let k be len + n.
+            //  b. If k < 0, let k be 0.
+            var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+            function sameValueZero(x, y) {
+                return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+            }
+
+            // 7. Repeat, while k < len
+            while (k < len) {
+                // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+                // b. If SameValueZero(searchElement, elementK) is true, return true.
+                if (sameValueZero(o[k], searchElement)) {
+                    return true;
+                }
+                // c. Increase k by 1.
+                k++;
+            }
+
+            // 8. Return false
+            return false;
         };
     }
 
